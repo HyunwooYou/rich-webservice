2278810c077ca738df56242ab09a63e78cc35dca
hyun-woo-yoo <hyun-woo.yoo@hp.com>
2021-02-19 21:29:51 +0900

    BaseTimeEntity

--- src/test/java/com/rich/domain/PostsRepositoryTest.java
+++ src/test/java/com/rich/domain/PostsRepositoryTest.java
@@ -1,17 +1,17 @@
 package com.rich.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.rich.domain.posts.Posts;
 import com.rich.domain.posts.PostsRepository;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -24,6 +24,7 @@ public class PostsRepositoryTest {
   public void cleanup() {
     postsRepository.deleteAll();
   }
+
 
   @Test
   public void test_get_board_content() {
@@ -44,5 +45,25 @@ public class PostsRepositoryTest {
     Posts posts = postsList.get(0);
     assertThat(posts.getTitle()).isEqualTo(title);
     assertThat(posts.getContent()).isEqualTo(content);
+  }
+
+  @Test
+  public void test_BaseTimeEntity() {
+    //given
+    LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0, 0, 0);
+    postsRepository.save(Posts.builder()
+        .title("title")
+        .content("content")
+        .author("author").build());
+
+    //when
+    List<Posts> postsList = postsRepository.findAll();
+
+    //then
+    Posts posts = postsList.get(0);
+
+    System.out.print(">>>>>>>>>>> createDate=" + posts.getCreatedDate() + ", modifiedDate=" + posts.getModifiedDate());
+    assertThat(posts.getCreatedDate()).isAfter(now);
+    assertThat(posts.getModifiedDate()).isAfter(now);
   }
 }
