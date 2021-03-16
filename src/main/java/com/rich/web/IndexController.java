2b68c85eb1a0dfd662cfbe9848d0466d36051d43
hyun-woo-yoo <hyun-woo.yoo@hp.com>
2021-03-16 21:30:44 +0900

    Update spring-session-jdbc

--- src/main/java/com/rich/web/IndexController.java
+++ src/main/java/com/rich/web/IndexController.java
@@ -1,9 +1,9 @@
 package com.rich.web;
 
+import com.rich.config.auth.LoginUser;
 import com.rich.config.auth.dto.SessionUser;
 import com.rich.service.posts.PostsService;
 import com.rich.web.dto.PostsResponseDto;
-import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,12 +15,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class IndexController {
 
   private final PostsService postsService;
-  private final HttpSession httpSession;
 
   @GetMapping("/")
-  public String index(Model model) {
+  public String index(Model model, @LoginUser SessionUser user) {
     model.addAttribute("posts", postsService.findAllDesc());
-    SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
     if (user != null) {
       model.addAttribute("userName", user.getName());
